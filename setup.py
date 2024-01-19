5fc64ef104db20f824c3d82047d9ad2495dedbd5
MouadEt-tali <mouad.et-tali@aqsone.com>
2024-01-19 18:46:35 +0100

    adds termui example

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='aliases',
     version='1.0',
-    py_modules=['hello_world','restaurant','inout','naval',"aliases",'colors'],
+    py_modules=['hello_world','restaurant','inout','naval',"aliases",'colors','termui'],
     install_requires=[
         'Click'
     ],
@@ -15,5 +15,7 @@ setup(
         naval=naval:cli
         aliases=aliases:cli
         colors=colors:cli
+        termui=termui:cli
+
 '''
 )
